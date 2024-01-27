1186c0089934936136037b2d4b046b09997268f2
Phoenix <aadilsiddiqui2016+github@gmail.com>
2024-01-27 15:28:23 +0530

    Front page added

---
+++ New folder/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Hide loader after page loads
+    setTimeout(function() {
+      document.getElementById('loader').style.display = 'none';
+    }, 2000);
+  });
